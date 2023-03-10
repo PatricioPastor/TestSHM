716aad67657200306f9ae17898c1435e711e2ad9
Patricio <patricio.pastor07@gmail.com>
2023-03-09 23:00:18 -0300

    terminado

--- form/main.js
+++ form/main.js
@@ -1,14 +1,20 @@
 const formART = document.getElementById("ART");
 const ART = document.getElementById("Form");
+const formuART = document.getElementById("FormART");
+const formuAltaART = document.getElementById("Formu");
 
 const focusART = ()=> {
     formART.classList.add("focus");
     ART.classList.remove("focus");
+    formuART.classList.remove("hide");
+    formuAltaART.classList.add("hide");
 }
 
 const focusFormTwo = ()=> {
     formART.classList.remove("focus");
     ART.classList.add("focus");
+    formuAltaART.classList.remove("hide");
+    formuART.classList.add("hide");
 }
 
 
